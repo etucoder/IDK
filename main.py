43ee2b66ae89347c6196d3559d080abc15d2774b
etucoder <ethu2020m@gmail.com>
2026-09-21 17:35:45 -0500

    Made the actual landing page

--- main.py
+++ main.py
@@ -11,15 +11,31 @@ def read_root():
         <head>
             <title>Render Python Test</title>
             <style>
-                body { font-family: sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; background-color: #f4f4f9; }
+                .title { display : flex ; justify-content : center;}
                 .card { padding: 40px; background: white; border-radius: 8px; box-shadow: 0 4px 6px rgba(0,0,0,0.1); text-align: center; }
+                .credentials { display : flex ; justify-content : center;}
+                .input-group {display : flex; align-items: center; margin-bottom : 6px}
+                label {width : 90px; text-align :left;}
+                input {flex : 1; padding : 3px ; border : 1px solid #000000; }
                 h1 { color: #4F46E5; }
             </style>
         </head>
         <body>
-            <div class="card">
-                <h1>🚀 It Works!</h1>
-                <p>Your Python FastAPI test app is running successfully on Render.</p>
+            <div class="title" style="">
+                <h1>Welcome to IDK</h1>
+            </div>
+            <div class = "credentials">
+              <form action="submit-credentials" method = "post">
+                <div class = "input-group">
+                  <label for="username-input">Username : </label>
+                  <input type = "text" id = "username-input" name = "username">
+                </div>
+                
+                <div class = "input-group">
+                  <label for="password-input">Password: </label>
+                  <input type = "password" id = "password-input" name = "password">
+                </div>
+              </form>
             </div>
         </body>
     </html>
